c89c2b71d10a336eee4d436ea14c85b90fe4ed42
Wade Wegner <wade.wegner@gmail.com>
2014-01-02 13:11:37 -0700

    Updated with functional tests

--- src/CommonToolkitForNET.FunctionalTests/AuthClientTests.cs
+++ src/CommonToolkitForNET.FunctionalTests/AuthClientTests.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace CommonToolkitForNET.FunctionalTests
 {
     public class AuthClientTests
     {
+        private static string _tokenRequestEndpointUrl = ConfigurationSettings.AppSettings["TokenRequestEndpointUrl"];
+        private static string _securityToken = ConfigurationSettings.AppSettings["SecurityToken"];
+        private static string _consumerKey = ConfigurationSettings.AppSettings["ConsumerKey"];
+        private static string _consumerSecret = ConfigurationSettings.AppSettings["ConsumerSecret"];
+        private static string _username = ConfigurationSettings.AppSettings["Username"];
+        private static string _password = ConfigurationSettings.AppSettings["Password"] + _securityToken;
+
+        [Test]
+        public async void Auth_ValidCreds_HasApiVersion()
+        {
+            var auth = new AuthClient();
+            Assert.IsNotNullOrEmpty(auth.ApiVersion);
+        }
+
+        [Test]
+        public async void Auth_ValidCreds_HasAccessToken()
+        {
+            var auth = new AuthClient();
+            await auth.Authenticate(_consumerKey, _consumerSecret, _username, _password, _tokenRequestEndpointUrl);
+
+            Assert.IsNotNullOrEmpty(auth.AccessToken);
+        }
+
+        [Test]
+        public async void Auth_ValidCreds_HasInstanceUrl()
+        {
+            var auth = new AuthClient();
+            await auth.Authenticate(_consumerKey, _consumerSecret, _username, _password, _tokenRequestEndpointUrl);
+
+            Assert.IsNotNullOrEmpty(auth.InstanceUrl);
+        }
     }
 }
