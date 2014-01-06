c00e0ac487b41b613f879372190d19169e29bb6c
Wade Wegner <wade.wegner@gmail.com>
2014-01-06 10:09:10 -0700

    Added new HttpGet to return node objects

--- src/CommonToolkitForNET/ToolkitHttpClient.cs
+++ src/CommonToolkitForNET/ToolkitHttpClient.cs
@@ -66,6 +66,39 @@ namespace CommonToolkitForNET
             }
         }
 
+        public async Task<T> HttpGet<T>(string urlSuffix, string nodeName)
+        {
+            var url = Common.FormatUrl(urlSuffix, _instanceUrl, _apiVersion);
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(string.Concat(_userAgent, "/", _apiVersion));
+
+                var request = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri(url),
+                    Method = HttpMethod.Get
+                };
+
+                request.Headers.Add("Authorization", "Bearer " + _accessToken);
+
+                var responseMessage = await client.SendAsync(request);
+                var response = await responseMessage.Content.ReadAsStringAsync();
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var jObject = JObject.Parse(response);
+                    var jToken = jObject.GetValue(nodeName);
+
+                    var r = JsonConvert.DeserializeObject<T>(jToken.ToString());
+                    return r;
+                }
+
+                var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(response);
+                throw new ForceException(errorResponse.errorCode, errorResponse.message);
+            }
+        }
+
         public async Task<T> HttpPost<T>(object inputObject, string urlSuffix)
         {
             var url = Common.FormatUrl(urlSuffix, _instanceUrl, _apiVersion);
