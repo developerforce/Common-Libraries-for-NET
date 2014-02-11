9d849df93bd8d512050195aecb496bc816d2341c
Wade Wegner <wade.wegner@gmail.com>
2014-02-11 10:44:56 -0700

    Added DescribeGlobalResult for sobjects query

--- src/CommonLibrariesForNET.FunctionalTests/CommonTests.cs
+++ src/CommonLibrariesForNET.FunctionalTests/CommonTests.cs
@@ -17,6 +17,23 @@ namespace Salesforce.Common.FunctionalTests
         private static string _username = ConfigurationSettings.AppSettings["Username"];
         private static string _password = ConfigurationSettings.AppSettings["Password"] + _securityToken;
 
+
+        [Test]
+        public async void Query_Objects()
+        {
+            const string userAgent = "common-libraries-dotnet";
+
+            var auth = new AuthenticationClient();
+            await auth.UsernamePasswordAsync(_consumerKey, _consumerSecret, _username, _password, userAgent, _tokenRequestEndpointUrl);
+
+            var serviceHttpClient = new ServiceHttpClient(auth.InstanceUrl, auth.ApiVersion, auth.AccessToken, userAgent, new HttpClient());
+
+            var response = await serviceHttpClient.HttpGetAsync<DescribeGlobalResult<dynamic>>(string.Format("sobjects"));
+
+            Assert.IsTrue(response.maxBatchSize > 0);
+            Assert.IsTrue(response.sobjects.Count > 0);
+        }
+
         [Test]
         public async void Query_Select_Account()
         {
